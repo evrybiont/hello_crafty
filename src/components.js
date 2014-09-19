440aceec930b7ea7edad8f63339a7e254cec023b
evrybiont <evrybiont@gmail.com>
2014-09-19 19:01:51 +0300

    Add animation

--- src/components.js
+++ src/components.js
@@ -36,10 +36,29 @@ Crafty.c("Bush", {
 
 Crafty.c("PlayerCharacter", {
   init: function(){
-    this.requires("Actor, Fourway, Collision, spr_player")
+    this.requires("Actor, Fourway, Collision, spr_player, SpriteAnimation")
       .fourway(4)
       .stopOnSolid()
-      .onHit("Village", this.visitVillage);
+      .onHit("Village", this.visitVillage)
+      .animate("PlayerMovingUp",    0, 0, 2)
+      .animate("PlayerMovingRight", 0, 1, 2)
+      .animate("PlayerMovingDown",  0, 2, 2)
+      .animate("PlayerMovingLeft",  0, 3, 2);
+
+    var animation_speed = 8;
+    this.bind("NewDirection", function(data){
+      if (data.x > 0){
+        this.animate("PlayerMovingRight", animation_speed, -1);
+      } else if (data.x < 0){
+        this.animate("PlayerMovingLeft", animation_speed, -1);
+      } else if (data.y > 0){
+        this.animate("PlayerMovingDown", animation_speed, -1);
+      } else if (data.y < 0){
+        this.animate("PlayerMovingUp", animation_speed, -1);
+      } else {
+        this.stop();
+      }
+    })
   },
 
   stopOnSolid: function() {
